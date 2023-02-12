d6443f5dacd367b2b648dc9a27a78aa2e603cf93
8ctopus <hello@octopuslabs.io>
2023-02-12 12:16:01 +0400

    added: array access

--- src/DeoxyriboNucleicAcid.php
+++ src/DeoxyriboNucleicAcid.php
@@ -4,10 +4,17 @@ declare(strict_types=1);
 
 namespace Oct8pus\DNA;
 
+use ArrayAccess;
+use Exception;
 use Oct8pus\DNA\NucleoBase;
 use Stringable;
 
-class DeoxyriboNucleicAcid implements Stringable
+class DeoxyriboNucleicAcidException extends Exception
+{
+
+}
+
+class DeoxyriboNucleicAcid implements Stringable, ArrayAccess
 {
     private array $strand;
 
@@ -42,5 +49,58 @@ class DeoxyriboNucleicAcid implements Stringable
     {
         $this->strand = [];
         return $this;
+    }
+
+    public function offsetGet(mixed $offset) : NucleoBase
+    {
+        if (gettype($offset) !== 'integer') {
+            throw new DeoxyriboNucleicAcidException('offset must be integer');
+        }
+
+        if ($offset >= $this->length() || $offset < 0) {
+            throw new DeoxyriboNucleicAcidException('out of range');
+        }
+
+        return $this->strand[$offset];
+    }
+
+    public function offsetSet(mixed $offset, mixed $value) : void
+    {
+        if (gettype($offset) !== 'integer') {
+            throw new DeoxyriboNucleicAcidException('offset must be integer');
+        }
+
+        if ($offset >= $this->length() || $offset < 0) {
+            throw new DeoxyriboNucleicAcidException('out of range');
+        }
+
+        if ($value instanceof NucleoBase) {
+            $this->strand[$offset] = $value;
+        } else {
+            throw new DeoxyriboNucleicAcidException('value must be NucleoBase');
+        }
+    }
+
+    public function offsetExists(mixed $offset) : bool
+    {
+        if (gettype($offset) !== 'integer') {
+            throw new DeoxyriboNucleicAcidException('offset must be integer');
+        }
+
+        if ($offset < 0) {
+            throw new DeoxyriboNucleicAcidException('offset must be positive');
+        }
+
+        if ($offset < $this->length()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function offsetUnset(mixed $offset) : void
+    {
+        throw new DeoxyriboNucleicAcidException('not implemented');
+        //unset($this->strand[$offset]);
     }
 }
--- tests/DeoxyriboNucleicAcidTest.php
+++ tests/DeoxyriboNucleicAcidTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Oct8pus\DNA\DeoxyriboNucleicAcid;
+use Oct8pus\DNA\DeoxyriboNucleicAcidException;
 use Oct8pus\DNA\NucleoBase;
 use PHPUnit\Framework\TestCase;
 
@@ -12,7 +13,7 @@ use PHPUnit\Framework\TestCase;
  */
 final class DeoxyriboNucleicAcidTest extends TestCase
 {
-    public function test() : void
+    public function testBasic() : void
     {
         $dna = (new DeoxyriboNucleicAcid());
 
@@ -28,8 +29,83 @@ final class DeoxyriboNucleicAcidTest extends TestCase
         static::assertSame(4, $dna->length());
         static::assertSame('ATGC', (string) $dna);
 
-        $dna->truncate();
+        // get offset
+        static::assertSame(NucleoBase::Guanine, $dna[2]);
 
+        // set offset
+        $dna[2] = NucleoBase::Cytosine;
+        static::assertSame('ATCC', (string) $dna);
+
+        // offset exists
+        static::assertSame(true, isset($dna[3]));
+        static::assertSame(false, isset($dna[4]));
+
+        $dna->truncate();
         static::assertSame(0, $dna->length());
+        static::assertSame(false, isset($dna[0]));
+    }
+
+    public function offsetGetValues() : array
+    {
+       return [
+           [-1],
+           ['a'],
+       ];
+    }
+
+    /**
+     * @dataProvider offsetGetValues
+     */
+    public function testOffsetGetExceptions(mixed $badValue): void
+    {
+        static::expectException(DeoxyriboNucleicAcidException::class);
+
+        $dna = (new DeoxyriboNucleicAcid());
+
+        $dna[$badValue];
+    }
+
+    /**
+     * @dataProvider offsetGetValues
+     */
+    public function testOffsetExistsExceptions(mixed $badValue): void
+    {
+        static::expectException(DeoxyriboNucleicAcidException::class);
+
+        $dna = (new DeoxyriboNucleicAcid());
+
+        isset($dna[$badValue]);
+    }
+
+    public function offsetSetValues() : array
+    {
+       return [
+           [-1],
+           ['a'],
+           [0],
+       ];
+    }
+
+    /**
+     * @dataProvider offsetSetValues
+     */
+    public function testOffsetSetExceptions(mixed $badValue): void
+    {
+        static::expectException(DeoxyriboNucleicAcidException::class);
+
+        $dna = (new DeoxyriboNucleicAcid())
+            ->add(NucleoBase::Adenine);
+
+        $dna[$badValue] = 'a';
+    }
+
+    public function testOffsetUnsetExceptions(): void
+    {
+        static::expectException(DeoxyriboNucleicAcidException::class);
+
+        $dna = (new DeoxyriboNucleicAcid())
+            ->add(NucleoBase::Adenine);
+
+        $dna[0] = null;
     }
 }
